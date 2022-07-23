585bbbb43ae56eb8a146ba363c75cec6645bd7dc
tapir2342 <github@tapir2342.com>
2022-07-24 01:37:20 +0200

    Don't quit game on ESC in browser

--- main.gd
+++ main.gd
@@ -2,5 +2,5 @@ extends Node2D
 
 
 func _input(event: InputEvent):
-	if event.is_action_pressed("ui_cancel"):
+	if event.is_action_pressed("ui_cancel") and OS.get_name() != "HTML5":
 		get_tree().quit()
